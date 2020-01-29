e8cd7b586a4815aae8200a6ebc6a60dd239a26d1
Kagaroat <tayyab.razzaq401@gmail.com>
2020-01-29 12:45:02 +0500

    feat: added fail test case of delete question api without authorization

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -449,6 +449,18 @@ class TriviaTestCase(unittest.TestCase):
             json_data.get('message'), ERROR_MESSAGES[STATUS_NOT_FOUND]
         )
 
+    def test_delete_question_failed_no_auth(self):
+        """
+        Fail case of delete question test case without authorization.
+
+        :return:
+        """
+        response = self.client().delete('/questions/1')
+        json_data = response.get_json()
+        self.assertEqual(response.status_code, STATUS_UNAUTHORIZED)
+        self.assertEqual(json_data.get('success'), False)
+        self.assertEqual(json_data.get('message'), MISSING_AUTHORIZATION)
+
     def test_play_quiz_success_member_role(self):
         """
         Success case for play quiz api with member.
