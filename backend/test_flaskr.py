66192dc233b695c626cb9011464f317787785368
Kagaroat <tayyab.razzaq401@gmail.com>
2020-01-29 12:45:18 +0500

    feat: added fail test case of delete question api with no bearer token

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -461,6 +461,18 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(json_data.get('success'), False)
         self.assertEqual(json_data.get('message'), MISSING_AUTHORIZATION)
 
+    def test_delete_question_failed_no_bearer_token(self):
+        """
+        Fail case of delete question test case with no bearer token.
+
+        :return:
+        """
+        response = self.client().delete('/questions/1', headers=self.no_bearer_token)
+        json_data = response.get_json()
+        self.assertEqual(response.status_code, STATUS_UNAUTHORIZED)
+        self.assertEqual(json_data.get('success'), False)
+        self.assertEqual(json_data.get('message'), MISSING_BEARER)
+
     def test_play_quiz_success_member_role(self):
         """
         Success case for play quiz api with member.
